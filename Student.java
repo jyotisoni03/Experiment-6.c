13681fbc62d27ad58096371a71b3dbd0efb530c1
Jyoti Soni <168131802+jyotisoni03@users.noreply.github.com>
2025-11-04 15:37:54 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public Student() {}
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+}
